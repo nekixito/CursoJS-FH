46019c6ca6b90750b29f81574cee092c0e99a0ab
Miguel Nequiz <nekixito@gmail.com>
2024-02-29 18:21:20 -0600

    Introducción a la consola

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -3,11 +3,16 @@
 
 let a = 10, 
     b = 20, 
-    c = 10, 
-    d = 10, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
     x = a + b;
 
+    const saludo = c + d;
 
-console.log(x);
+// console.log('%c Mis variables','color:blue; font-weight: bold');
+// console.log({a});
+// console.log({b});
+// console.log({c});
 
+//console.table({a, b, c, d, x});
 
