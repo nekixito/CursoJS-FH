fac2a704baeddf6c6e741d434a2fea0de7e193f1
Miguel Nequiz <nekixito@gmail.com>
2024-03-07 16:35:15 -0600

    Prompt, confirm y alert

---
+++ 01-fundamentos/assets/js/alerts.js
@@ -0,0 +1,16 @@
+
+//alert('Hola mundo');
+
+//let nombre = prompt('¿Cuál es tu nombre?','Sin nombre');
+//console.log(nombre);
+//console.log('****' + nombre + "****");
+
+//confirm, alert y prompt son de window del navegador
+/*
+const seleccion = confirm('¿Esta seguro de borrar esto?');
+console.log(seleccion);
+*/
+
+
+//global se puede ver con node
+console.log(global);
