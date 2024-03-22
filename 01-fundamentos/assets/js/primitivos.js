79b0d3a6687210a9b3891d2f08b07559471ac125
Miguel Nequiz <nekixito@gmail.com>
2024-03-21 18:19:14 -0600

    Introducción general a los datos primitivos

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,40 @@
+let nombre = 'Peter Parker';
+console.log(nombre);
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tia May";
+console.log(typeof nombre);
+nombre = `Tia May`;
+console.log(typeof nombre);
+
+
+nombre = 123;
+console.log(nombre)
+console.log(typeof nombre);
+
+let esMarvel = true;
+console.log(esMarvel);
+console.log(typeof esMarvel);
+
+let edad = 38.001;
+console.log(edad);
+console.log(typeof edad);
+
+let superPoder;
+console.log(typeof superPoder);
+
+let soyNull = null;
+console.log(soyNull);
+console.log(typeof soyNull);
+
+let symbol1 = Symbol('a');
+console.log(symbol1);
+console.log(typeof symbol1);
+
+let symbol2 = Symbol('a');
+console.log(symbol2);
+console.log(typeof symbol2);
+
+console.log(symbol1 == symbol2);
