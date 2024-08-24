42e27a37c75666181ac843b632c82fa24c3cc1c1
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2024-08-23 18:40:52 -0600

    Introducción a variables y comentarios

--- 01-FUNDAMENTOS/app.js
+++ 01-FUNDAMENTOS/app.js
@@ -1,2 +1,12 @@
 
-console.log(console.log('Hola Mundo'));
+// console.log(console.log('Hola Mundo'));
+
+let a = 10, 
+    b = 10, 
+    c = 10, 
+    d = 10, 
+    x = a + b;
+
+
+console.log(x);
+
