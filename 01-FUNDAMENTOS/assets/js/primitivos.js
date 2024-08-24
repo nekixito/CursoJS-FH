fa967f91844010df056cbae1a1c0a3ca7ac1d129
Antony Miguel Nequiz <miguel@MacBook-Air-de-Antony.local>
2024-08-24 08:11:18 -0600

    Introducción general a los tipos primitivos

---
+++ 01-FUNDAMENTOS/assets/js/primitivos.js
@@ -0,0 +1,34 @@
+let nombre = 'Peter Parker';
+console.log( nombre );
+
+nombre = 'Ben Parker';
+console.log( nombre );
+
+nombre = "Tía May";
+nombre = `Tía May`;
+
+console.log( typeof nombre );
+
+nombre = 123;
+console.log( typeof nombre );
+
+let esMarvel = true;
+console.log( typeof esMarvel );
+
+let edad = 39;
+console.log( typeof edad);
+
+edad = 39.0001;
+console.log( typeof edad);
+
+let superPoder;
+console.log( typeof superpoder); //???
+
+let soyNull = null;
+console.log( typeof soyNull );
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+console.log( typeof symbol1 );
+
+console.log( symbol1 === symbol2 );
