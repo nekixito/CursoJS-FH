0912bb90d7d9450e982210a36aa2934f6fc4f778
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2024-08-24 00:04:32 -0600

    var, prompt, confirm y alert

---
+++ 01-FUNDAMENTOS/assets/js/alerts.js
@@ -0,0 +1,10 @@
+// alert('Hola Mundo');
+
+//let nombre = prompt('Cuál es tu nombre','Sin nombre');
+//let nombre = prompt('Cuál es tu nombre');
+//console.log(nombre);
+
+//const seleccion = confirm('¿Estas seguro de borrar esto?');
+//console.log(seleccion);
+
+console.log(global);
---
+++ 01-FUNDAMENTOS/assets/js/app.js
@@ -0,0 +1,23 @@
+
+// console.log(console.log('Hola Mundo'));
+// alert('Hola desde app.js');
+
+let a = 10, 
+    b = 20, 
+    c = 'Hola ', 
+    d = 'Spiderman', 
+    x = a + b;
+
+const saludo = c + d;
+
+
+// console.log('%c Mis variables','color:blue; font-weight: bold');
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+c = 'Hola de nuevo';
+
+// console.table({b,c,d});
+
+var miNombre = 'Miguel';
