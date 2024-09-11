be3254bbbe7442bb4e8ae9f0a6ab6e6b6dbf5ba9
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2024-09-11 11:55:54 -0600

    Retorno de las funciones

--- 01-FUNDAMENTOS/assets/js/funciones.js
+++ 01-FUNDAMENTOS/assets/js/funciones.js
@@ -1,6 +1,7 @@
 function saludar( nombre ){
     console.log(arguments);
-    console.log('Hola ' + nombre);    
+    console.log('Hola ' + nombre);
+    //return 10;    
 }
 
 const saludar2 = function( nombre ){
@@ -15,7 +16,37 @@ const saludarFlecha2 = ( nombre ) => {
     console.log('Hola ' + nombre);
 }
 
-saludar('Antony', 39, true, 'México');
+
+
+const retornoDeSaludar = saludar('Antony', 39, true, 'México');
+console.log({retornoDeSaludar});
+
 saludar2('Miguel');
 saludarFlecha();
 saludarFlecha2('Antony');
+
+
+
+function sumar(a,b){
+    return a + b;
+}
+
+const sumar2 = (a,b) => {
+    return a+b;
+}
+
+const sumar3 = (a,b) =>  a + b;
+
+console.log(sumar(1,2));
+console.log(sumar2(3,2));
+console.log(sumar3(2,2));
+
+function getAleatorio(){
+    return Math.random();
+}
+
+//Convertir la función anterior en una función de flecha que no tenga llaves { }
+const getAleatorio2 = () => Math.random();
+
+console.log(getAleatorio());
+console.log(getAleatorio2());
